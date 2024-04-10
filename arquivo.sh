879fe1b9e2261962e692d1de35e77939eee70649
josy-lima <joseildabarbosa854@gmail.com>
2024-04-10 09:25:05 -0300

    Adiciona script executável

--- arquivo.sh
+++ arquivo.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+"Acorde todas as manhãs com um sorriso."
+"Esta é mais uma oportunidade que você tem para ser feliz."
+ "Seja seu próprio motor de ignição. O dia de hoje jamais voltará .... Não o desperdice!!!"
